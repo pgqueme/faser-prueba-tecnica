6162624514ed431559f943bc46df78c8fc4c4b81
Pablo Quemé <pgqueme@gmail.com>
2020-06-29 21:34:21 -0600

    Componente inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
+import { Tarea } from './tarea';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'faser-prueba-tecnica';
+	tareas: Tarea[];
+
+	constructor(
+        public service: AppService,
+	) { }
+	
+	ngOnInit() {
+		this.obtenerTareas();
+	}
+
+	async obtenerTareas() {
+		this.tareas = await this.service.obtenerTareas();
+	}
 }
